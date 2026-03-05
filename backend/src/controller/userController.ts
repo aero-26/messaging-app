698bc37e65a42870b9da9814bb9711b88a31df34
Shubhankar <shubhankar@teamco.design>
2026-03-05 15:50:49 +0530

    chore: Remove console log from getAllUser function in userController

--- backend/src/controller/userController.ts
+++ backend/src/controller/userController.ts
@@ -4,7 +4,6 @@ import { StatusCodes } from "http-status-codes";
 export const userController = {
   async getAllUser(req: Request, res: Response) {
     const userData = await fetch("https://dummyjson.com/users");
-    console.log(userData);
     res.status(StatusCodes.OK).send(await userData.json());
   },
 };
