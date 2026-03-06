3065ee5aeb1c1e04a5e148b1052c98dd40a36e0a
Shubhankar <shubhankar@teamco.design>
2026-03-06 20:20:51 +0530

    refactor: clean up App component and standardize import formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,7 @@
-import { Button } from "@mui/material";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <span className="bg-red-500">Hello</span>
-      <Button variant="outlined" className="bg-pink-500">
-        Text
-      </Button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
