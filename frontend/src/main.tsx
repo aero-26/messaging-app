6c8130164224cffe0af8ef2d38b13ad3aaaa32d3
Shubhankar <shubhankar@teamco.design>
2026-03-07 20:29:31 +0530

    feat: add @mui/icons-material dependency and define theme structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
+export const theme = {
+  dark: {
+    notificationColor: "#40D169",
+    backgroundColor: "#0a0a0a",
+    sendBubble: "#145345",
+    receiveBubble: "#363636",
+    fontPrimary: "#FFFFFF",
+    fontSecondary: "#767676",
+  },
+  light: {
+    notificationColor: "#40D169",
+    backgroundColor: "#FFFFFF",
+    sendBubble: "#E3FDD4",
+    receiveBubble: "#FFFFFF",
+    fontPrimary: "#0a0a0a",
+    fontSecondary: "#767676",
+  },
+};
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
