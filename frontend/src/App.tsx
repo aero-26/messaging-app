1ddb8f0110112fe54afbf1b7f7067cb07b3cccda
Shubhankar <shubhankar@teamco.design>
2026-03-08 12:55:47 +0530

    feat: implement theme switching and define color palette for light and dark modes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import { Button } from "@mui/material";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [themeColor, setThemeColor] = useState("light");
+
+  const switchLightAndDarkMode = () =>
+    themeColor === "dark" ? setThemeColor("light") : setThemeColor("dark");
+
   return <></>;
 }
 
